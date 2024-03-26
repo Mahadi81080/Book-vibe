ad8676924c1d7f8eb643f9325ba94385c4d8f52e
Mahadi810803 <mahedihasan81080@gmail.com>
2024-03-26 14:42:07 +0600

    some section complite

--- src/Components/ReadBook/ReadBook.jsx
+++ src/Components/ReadBook/ReadBook.jsx
@@ -70,52 +70,6 @@ const ReadBook = ({ book }) => {
           </div>
         </div>
       </section>
-      {/* <div className="hero my-4 border-2 rounded-lg">
-        <div className="hero-content flex flex-col lg:flex-row">
-          <img src={image} className="max-w-sm h-80 rounded-lg shadow-lg " />
-          <div className="flex-1">
-            <h1 className="text-5xl font-bold"> {bookName}</h1>
-            <p className="py-6">By : {author}</p>
-            <div className="flex text-green-400 gap-6 ">
-              <h3 className="text-lg font-semibold text-black">Tag </h3>
-              <h2 className="text-sm bg-slate-100 p-1 rounded-full px-2">
-                {tags[0]}
-              </h2>
-              <h2 className="text-sm bg-slate-100 p-1 rounded-full px-2">
-                {tags[1]}
-              </h2>
-              <div className="flex gap-2 justify-center items-center text-black">
-                <CiLocationOn />
-                <p>Year of Publishing:</p>
-                <h3>{yearOfPublishing}</h3>
-              </div>
-            </div>
-            <div className="flex justify-between items-center border-b-2 py-4">
-              <div className="flex justify-center items-center gap-2">
-                <FaUserGroup />
-                <p>Publisher:</p>
-                <h3>{publisher}</h3>
-              </div>
-              <div className="flex justify-center items-center gap-2">
-                <MdOutlineContactPage />
-                <h3>Page</h3>
-                <h3>{totalPages}</h3>
-              </div>
-            </div>
-            <div className="flex items-center gap-6 mt-4">
-              <div className="bg-[#e0eeff] text-xs text-blue-400 rounded-full p-2">
-                <h3>Category:{category}</h3>
-              </div>
-              <div className="text-xs bg-orange-100 text-orange-400 rounded-full p-2">
-                <h3>Rating:{rating}</h3>
-              </div>
-              <button className="text-xs p-2 bg-green-600 text-white rounded-full">
-                View details
-              </button>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
